7cccee915892a522b7b7640fad0d49b7d0d2fb84
v4rgas <jota.montecino@gmail.com>
2025-05-06 20:18:03 +0200

    ADD: n8n

---
+++ n8n/prod.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+set -eu -o pipefail
+
+## General internal vars
+N8N_VERSION="1.80.3"
+IMAGE_REPO="${CI_REGISTRY_IMAGE:-docker.io/trashnochados/n8n}"
+IMAGE_TAG="${CI_COMMIT_REF_SLUG:-1.80.3}"
+
+CI_REGISTRY_BASE_IMAGE=docker.io/python:3.9.21-alpine3.21
+
+echo ""
+echo "Building $IMAGE_REPO:$IMAGE_TAG"
+
+. $PWD/base/prod.sh
+
+buildah config \
+    --env N8N_VERSION="${N8N_VERSION}" \
+    --env NODE_ICU_DATA=/usr/local/lib/node_modules/full-icu \
+    --env NODE_VERSION=22.14.0 \
+    --env NODE_VERSION_CHECKSUM=87f163387ac85df69df6eeb863a6b6a1aa789b49cda1c495871c0fe360634db3 \
+    --env YARN_VERSION=1.22.22 \
+    $container
+
+brun /bin/sh -c "apk add --no-cache curl libstdc++ && apk add --no-cache --virtual .build-deps curl"
+
+echo ""
+bprint '1. Installing Node: "$NODE_VERSION"'
+echo "###################"
+brun /bin/sh -c 'ARCH= OPENSSL_ARCH="linux*" && alpineArch="$(apk --print-arch)" && case "${alpineArch##*-}" in x86_64) ARCH="x64" CHECKSUM="${NODE_VERSION_CHECKSUM}" OPENSSL_ARCH=linux-x86_64;; x86) OPENSSL_ARCH=linux-elf;; aarch64) OPENSSL_ARCH=linux-aarch64;; arm*) OPENSSL_ARCH=linux-armv4;; ppc64le) OPENSSL_ARCH=linux-ppc64le;; s390x) OPENSSL_ARCH=linux-s390x;; *) ;; esac && if [ -n "${CHECKSUM}" ]; then set -eu; curl -fsSLO --compressed "https://unofficial-builds.nodejs.org/download/release/v$NODE_VERSION/node-v$NODE_VERSION-linux-$ARCH-musl.tar.xz"; echo "$CHECKSUM node-v$NODE_VERSION-linux-$ARCH-musl.tar.xz" | sha256sum -c - && tar -xJf "node-v$NODE_VERSION-linux-$ARCH-musl.tar.xz" -C /usr/local --strip-components=1 --no-same-owner && ln -s /usr/local/bin/node /usr/local/bin/nodejs; else echo "Building from source: https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION.tar.xz" && apk add --no-cache --virtual .build-deps-full binutils-gold g++ gcc gnupg libgcc linux-headers make python3 py-setuptools && export GNUPGHOME="$(mktemp -d)" && for key in 4ED778F539E3634C779C87C6D7062848A1AB005C 141F07595B7B3FFE74309A937405533BE57C7D57 74F12602B6F1C4E913FAA37AD3A89613643B6201 DD792F5973C6DE52C432CBDAC77ABFA00DDBF2B7 61FC681DFB92A079F1685E77973F295594EC4689 8FCCA13FEF1D0C2E91008E09770F7A9A5AE15600 C4F0DFFF4E8C1A8236409D08E73BC641CC11F4C8 890C08DB8579162FEE0DF9DB8BEAB4DFCF555EF4 C82FA3AE1CBEDC6BE46B9360C43CEC45C17AB93C 108F52B48DB57BB0CC439B2997B01419BD92F80A A363A499291CBBC940DD62E41F10027AF002F8B0 CC68F5A3106FF448322E48ED27F5E38D5B0A215F ; do gpg --batch --keyserver hkps://keys.openpgp.org --recv-keys "$key" || gpg --batch --keyserver keyserver.ubuntu.com --recv-keys "$key" ; done && curl -fsSLO --compressed "https://nodejs.org/dist/v$NODE_VERSION/node-v$NODE_VERSION.tar.xz" && curl -fsSLO --compressed "https://nodejs.org/dist/v$NODE_VERSION/SHASUMS256.txt.asc" && gpg --batch --decrypt --output SHASUMS256.txt SHASUMS256.txt.asc && gpgconf --kill all && rm -rf "$GNUPGHOME" && grep " node-v$NODE_VERSION.tar.xz\$" SHASUMS256.txt | sha256sum -c - && tar -xf "node-v$NODE_VERSION.tar.xz" && cd "node-v$NODE_VERSION" && ./configure && make -j$(getconf _NPROCESSORS_ONLN) V= && make install && apk del .build-deps-full && cd .. && rm -Rf "node-v$NODE_VERSION" && rm "node-v$NODE_VERSION.tar.xz" SHASUMS256.txt.asc SHASUMS256.txt; fi && rm -f "node-v$NODE_VERSION-linux-$ARCH-musl.tar.xz" && find /usr/local/include/node/openssl/archs -mindepth 1 -maxdepth 1 ! -name "$OPENSSL_ARCH" -exec rm -rf {} \; && apk del .build-deps && node --version && npm --version'
+
+echo ""
+bprint '2. Installing Yarn: "$YARN_VERSION"'
+echo "###################"
+brun /bin/sh -c 'apk add --no-cache --virtual .build-deps-yarn curl gnupg tar && export GNUPGHOME="$(mktemp -d)" && for key in 6A010C5166006599AA17F08146C2130DFD2497F5 ; do gpg --batch --keyserver hkps://keys.openpgp.org --recv-keys "$key" || gpg --batch --keyserver keyserver.ubuntu.com --recv-keys "$key" ; done && curl -fsSLO --compressed "https://yarnpkg.com/downloads/$YARN_VERSION/yarn-v$YARN_VERSION.tar.gz" && curl -fsSLO --compressed "https://yarnpkg.com/downloads/$YARN_VERSION/yarn-v$YARN_VERSION.tar.gz.asc" && gpg --batch --verify yarn-v$YARN_VERSION.tar.gz.asc yarn-v$YARN_VERSION.tar.gz && gpgconf --kill all && rm -rf "$GNUPGHOME" && mkdir -p /opt && tar -xzf yarn-v$YARN_VERSION.tar.gz -C /opt/ --no-same-owner --no-same-permissions && ln -s /opt/yarn-v$YARN_VERSION/bin/yarn /usr/local/bin/yarn && ln -s /opt/yarn-v$YARN_VERSION/bin/yarnpkg /usr/local/bin/yarnpkg && rm yarn-v$YARN_VERSION.tar.gz.asc yarn-v$YARN_VERSION.tar.gz && apk del .build-deps-yarn && yarn --version && rm -rf /tmp/*'
+
+
+echo "INSTALL BASIC DEPENDENCIES"
+brun /bin/sh -c 'apk add --no-cache --update git graphicsmagick tini tzdata'
+
+echo "INSTALL N8N"
+brun /bin/sh -c 'npm_config_user=root npm install -g full-icu n8n@${N8N_VERSION} && rm -rf /root /tmp/* /var/cache/apk/* && mkdir /root'
+
+echo "INSTALL FONT"
+brun /bin/sh -c 'apk --no-cache add --virtual fonts msttcorefonts-installer fontconfig && update-ms-fonts && fc-cache -f && apk del fonts && find /usr/share/fonts/truetype/msttcorefonts/ -type l -exec unlink {} \; && rm -rf /root /tmp/* /var/cache/apk/* && mkdir /root'
+
+echo "INSTALL FONTS"
+buildah copy --add-history $container 'n8n/entrypoint.sh' '/entrypoint.sh'
+
+echo "SET CONFIG"
+buildah config \
+    --workingdir=/data \
+    --entrypoint=/entrypoint.sh \
+    $container
+
+echo "PUSH"
+. $PWD/base/commit.sh
